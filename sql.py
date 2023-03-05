5a349b832164cb8011a0b3a6fa70720c03033f14
Parth Bansal <33196466+parthbansal05@users.noreply.github.com>
2023-03-05 07:53:08 +0530

    roll

--- sql.py
+++ sql.py
@@ -1,25 +1,23 @@
 import sqlite3
 import os
 import time
-import os
-import psycopg2
 
 
 class DB_Manager:
     def __init__(self):
-        self.conn = psycopg2.connect(os.environ["DATABASE_URL"])
-
+        self.conn = sqlite3.connect(
+            (str(os.path.dirname(os.path.abspath(__file__)))+'/config.db'))
 
     # record table creation
     def TableCreation(self):
-        self.conn.cursor().execute('''CREATE TABLE ITEM
-                (ID         SERIAL      PRIMARY KEY     ,
+        self.conn.execute('''CREATE TABLE ITEM
+                (ID         INTEGER     PRIMARY KEY     AUTOINCREMENT,
                 Name        TEXT        UNIQUE          NOT NULL,
                 Price       INTEGER     NOT NULL,
                 Description TEXT                        NOT NULL);''')
         print("ITEM Record table created successfully")
 
-        self.conn.cursor().execute('''CREATE TABLE ORDERS
+        self.conn.execute('''CREATE TABLE ORDERS
                 (Order_ID       INTEGER     NOT NULL,
                 Item_ID         INTEGER     NOT NULL,
                 User_ID         INTEGER     NOT NULL,
@@ -28,7 +26,6 @@ class DB_Manager:
                 Number          INTEGER     NOT NULL);''')
                 
         print("ORDERS Record table created successfully")
-        self.close_conn()
 
        
 
@@ -37,7 +34,6 @@ class DB_Manager:
     def QuarryAllItem(self):
         SUB = self.SqlQuarryExec("""select ID,Name,Price,Description
                                 from ITEM;""")
-        self.close_conn()
         return ([[s[0] for s in SUB], [s[1] for s in SUB], [s[2] for s in SUB], [s[3] for s in SUB]])
 
     def QuarryItemPrice(self):
@@ -48,93 +44,79 @@ class DB_Manager:
     def QuarryItemDescriptiom(self):
         SUB = self.SqlQuarryExec("""select Description
                                 from ITEM;""")
-        self.close_conn()
         return ([s[0] for s in SUB])
 
     def QuarryAllOrder(self):
         SUB = self.SqlQuarryExec("""select Order_ID,Item_ID,User_ID,Table_Number,Order_Status,Number
                                 from ORDERS
                                 order by ID asc;""")
-        self.close_conn()
         return ([[s[0] for s in SUB], [s[1] for s in SUB], [s[2] for s in SUB], [s[3] for s in SUB], [s[4] for s in SUB], [s[5] for s in SUB]])
 
     def QuarryOrderByUser_ID(self, ID):
         SUB = self.SqlQuarryExec("""select Order_ID,Item_ID,User_ID,Table_Number,Order_Status,Number
                                 from ORDERS
                                 where User_ID={0};""".format(ID))
-        self.close_conn()
         return ([[s[0] for s in SUB], [s[1] for s in SUB], [s[2] for s in SUB], [s[3] for s in SUB], [s[4] for s in SUB], [s[5] for s in SUB]])
    
  # adding to Record table
 
     def AddItem(self, Name, Price, Description):
         try:
-            self.conn.cursor().execute("INSERT INTO ITEM (Name, Price, Description) VALUES (\"{0}\",{1},\"{2}\")".format(
+            self.conn.execute("INSERT INTO ITEM (Name, Price, Description) VALUES (\"{0}\",{1},\"{2}\")".format(
                 Name, Price, Description))
             self.Commit()
         except:
             self.conn.rollback()
-        self.close_conn()    
 
     def AddOrder(self, Order_ID, Item_ID, User_ID, Table_Number, Order_Status,number):
         try:
-            self.conn.cursor().execute("INSERT INTO ORDERS (Order_ID,Item_ID,User_ID,Table_Number,Order_Status,Number) VALUES ({0}, {1},{2},{3},{4},{5})".format(
+            self.conn.execute("INSERT INTO ORDERS (Order_ID,Item_ID,User_ID,Table_Number,Order_Status,Number) VALUES ({0}, {1},{2},{3},{4},{5})".format(
                 Order_ID,Item_ID,User_ID,Table_Number,Order_Status,number))
             self.Commit()
         except:
             self.conn.rollback()
-        self.close_conn()    
 
     # removing from table
 
     def RemoveItembyItem_ID(self, Item_ID):
         try:
-            self.conn.cursor().execute("DELETE from ITEM where Item_ID={0}".format(Item_ID))
+            self.conn.execute("DELETE from ITEM where Item_ID={0}".format(Item_ID))
             self.Commit()
         except:
             self.conn.rollback()
-        self.close_conn()    
 
     def RemoveOrderbyItem_ID(self, ID):
         try:
-            self.conn.cursor().execute(
+            self.conn.execute(
                 "DELETE from ORDERS where Order_ID={0}".format(ID))
             self.Commit()
         except:
             self.conn.rollback()
-        self.close_conn()    
 
     def RemoveOrderbyUser_ID(self, ID):
         try:
-            self.conn.cursor().execute(
+            self.conn.execute(
                 "DELETE from ORDERS where User_ID={0}".format(ID))
             self.Commit()
         except:
-            self.conn.rollback()   
-        self.close_conn()      
+            self.conn.rollback()     
 
     def updateOrderNumber(self, Order_ID, number, Item_ID):
         try:
-            self.conn.cursor().execute(
+            self.conn.execute(
                 "UPDATE ORDERS set Number={0} where Order_ID={1} and Item_ID={2}".format(number, Order_ID, Item_ID))
             self.Commit()
         except:
             self.conn.rollback()
-        self.close_conn()    
     # Misc
 
     def SqlQuarryExec(self, quarry):
         c = self.conn.cursor()
         c.execute(quarry)
-        self.close_conn()
         return (c.fetchall())
 
     def Commit(self):
         self.conn.commit()
-        self.close_conn()
-
-    def close_conn(self):
-        self.conn.close()
 
     # def DropTable(self, table):
     #     stm = "DROP TABLE {0};".format(table)
