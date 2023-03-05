25187435a92f39db9aa0c6d1fb8760b1ccf92422
Parth Bansal <33196466+parthbansal05@users.noreply.github.com>
2023-03-05 07:03:02 +0530

    fixed .cursor()

--- sql.py
+++ sql.py
@@ -12,14 +12,14 @@ class DB_Manager:
 
     # record table creation
     def TableCreation(self):
-        self.conn.execute('''CREATE TABLE ITEM
+        self.conn.cursor().execute('''CREATE TABLE ITEM
                 (ID         INTEGER     PRIMARY KEY     AUTOINCREMENT,
                 Name        TEXT        UNIQUE          NOT NULL,
                 Price       INTEGER     NOT NULL,
                 Description TEXT                        NOT NULL);''')
         print("ITEM Record table created successfully")
 
-        self.conn.execute('''CREATE TABLE ORDERS
+        self.conn.cursor().execute('''CREATE TABLE ORDERS
                 (Order_ID       INTEGER     NOT NULL,
                 Item_ID         INTEGER     NOT NULL,
                 User_ID         INTEGER     NOT NULL,
@@ -64,7 +64,7 @@ class DB_Manager:
 
     def AddItem(self, Name, Price, Description):
         try:
-            self.conn.execute("INSERT INTO ITEM (Name, Price, Description) VALUES (\"{0}\",{1},\"{2}\")".format(
+            self.conn.cursor().execute("INSERT INTO ITEM (Name, Price, Description) VALUES (\"{0}\",{1},\"{2}\")".format(
                 Name, Price, Description))
             self.Commit()
         except:
@@ -72,7 +72,7 @@ class DB_Manager:
 
     def AddOrder(self, Order_ID, Item_ID, User_ID, Table_Number, Order_Status,number):
         try:
-            self.conn.execute("INSERT INTO ORDERS (Order_ID,Item_ID,User_ID,Table_Number,Order_Status,Number) VALUES ({0}, {1},{2},{3},{4},{5})".format(
+            self.conn.cursor().execute("INSERT INTO ORDERS (Order_ID,Item_ID,User_ID,Table_Number,Order_Status,Number) VALUES ({0}, {1},{2},{3},{4},{5})".format(
                 Order_ID,Item_ID,User_ID,Table_Number,Order_Status,number))
             self.Commit()
         except:
@@ -82,14 +82,14 @@ class DB_Manager:
 
     def RemoveItembyItem_ID(self, Item_ID):
         try:
-            self.conn.execute("DELETE from ITEM where Item_ID={0}".format(Item_ID))
+            self.conn.cursor().execute("DELETE from ITEM where Item_ID={0}".format(Item_ID))
             self.Commit()
         except:
             self.conn.rollback()
 
     def RemoveOrderbyItem_ID(self, ID):
         try:
-            self.conn.execute(
+            self.conn.cursor().execute(
                 "DELETE from ORDERS where Order_ID={0}".format(ID))
             self.Commit()
         except:
@@ -97,7 +97,7 @@ class DB_Manager:
 
     def RemoveOrderbyUser_ID(self, ID):
         try:
-            self.conn.execute(
+            self.conn.cursor().execute(
                 "DELETE from ORDERS where User_ID={0}".format(ID))
             self.Commit()
         except:
@@ -105,7 +105,7 @@ class DB_Manager:
 
     def updateOrderNumber(self, Order_ID, number, Item_ID):
         try:
-            self.conn.execute(
+            self.conn.cursor().execute(
                 "UPDATE ORDERS set Number={0} where Order_ID={1} and Item_ID={2}".format(number, Order_ID, Item_ID))
             self.Commit()
         except:
